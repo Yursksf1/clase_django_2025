c689028c43ef2dd007c46bdbf400e193ba1cb373
YursKsf1 <yurs.ksf@gmail.com>
2025-04-02 20:33:46 -0500

    define estructura de atractivos turisticos

--- myapp/views.py
+++ myapp/views.py
@@ -5,9 +5,18 @@ from django.shortcuts import render
 def index(request):
     mensaje = "Villa de los Caballeros y Monumento Nacional 12345"
     parques = [
-        "Parque Pincipal",
-        "Parque las nieves",
-        "Parque peralta"
+        {
+            "name": "Parque Pincipal",
+            "description": "Parque en el centro de giron con la iglesia principal"
+        },
+        {
+            "name": "Parque las nieves",
+            "description": "Parque con la iglesia de las nieves"
+        },
+        {
+            "name": "Parque peralta",
+            "description": "Parque mas tranquilo y se pueden comprar deliciosos helados!"
+        },
     ]
     print(parques)
     
