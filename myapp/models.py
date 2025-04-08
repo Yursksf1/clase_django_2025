5c213ffd2d73ca3365e5c4063b2fe51cf8b94df2
YursKsf1 <yurs.ksf@gmail.com>
2025-04-07 19:09:28 -0500

    definir modelo parque

--- myapp/models.py
+++ myapp/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class Park(models.Model):
+    name = models.CharField(
+        max_length=500,
+        verbose_name="Nombre Parque"
+    )
+    description = models.TextField(
+        verbose_name="Descripcion Parque"
+    )
--- myapp/views.py
+++ myapp/views.py
@@ -3,7 +3,7 @@ from django.shortcuts import render
 # Create your views here.
 
 def index(request):
-    mensaje = "Villa de los Caballeros y Monumento Nacional 12345"
+    mensaje = "Villa de los Caballeros y Monumento Nacional 1111"
     parques = [
         {
             "name": "Parque Pincipal",
