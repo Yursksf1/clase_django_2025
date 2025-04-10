793738785d83638101f96f4cc80a81bb949b9ff8
YursKsf1 <yurs.ksf@gmail.com>
2025-04-09 20:15:10 -0500

    definir ratio crecimiento poblacional

--- myapp/views.py
+++ myapp/views.py
@@ -17,11 +17,27 @@ def giron(request):
 
 def bucaramanga(request):
     parques =  Park.objects.filter(city='BUC').all()
-    history_poblations =  Historico_Poblacion.objects.all()
+    history_poblations_list = []
+    history_poblations =  Historico_Poblacion.objects.order_by("year").all()
+    ratio_growth = None
+    previous_poblation = history_poblations[0]
+    for history_poblation in history_poblations:
+        if previous_poblation.year != history_poblation.year:
+            ratio_growth = round((history_poblation.poblation / previous_poblation.poblation -1), 2)
+            previous_poblation = history_poblation
+        history_poblations_list.append(
+            {
+                "year": history_poblation.year,
+                "poblation": history_poblation.poblation,
+                "ratio_growth": ratio_growth,
+            }
+        )
+
+
     print(parques)
     
     context = {
         "parques": parques,
-        "history_poblations": history_poblations,
+        "history_poblations": history_poblations_list,
     }
     return render(request, "bucaramanga.html", context)
